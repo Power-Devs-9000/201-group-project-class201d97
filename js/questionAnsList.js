b1e3c9d95158dff9c028704d3cca91c286dc3c69
ReeceRenninger <renningerreece@gmail.com>
2023-02-28 13:39:37 -0800

    working question function, updated question object

--- js/questionAnsList.js
+++ js/questionAnsList.js
@@ -2,13 +2,58 @@
 // variable being called on reece.js
 let questionList = {
   one: {
-    questionOne: 'This is our first test question',
-    questionOnechoices: ['a', 'b', 'c', 'd'],
+    question: 'This is our first test question',
+    questionChoices: ['a', 'b', 'c', 'd','e'],
     correct: 'b'
   },
 
   two: {
     question: 'Q2',
-    questionTwochoices: ['a2, b2, c2, d2']
-  }
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+
+  three: {
+    question: 'Q3',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+
+  four: {
+    question: 'Q4',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+
+  five: {
+    question: 'Q5',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+
+  six: {
+    question: 'Q4',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+  seven: {
+    question: 'Q5',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+  eight: {
+    question: 'Q5',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+  nine: {
+    question: 'Q5',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
+  ten: {
+    question: 'Q5',
+    questionChoices: ['a2', 'b2', 'c2', 'd2','e2'],
+    correct: 'a2'
+  },
 };
