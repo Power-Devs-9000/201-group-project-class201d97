6724fc7bb2b1688eb430733e285c71937f5c17d7
Ryan Eastman <rking1323@gmail.com>
2023-02-28 21:51:08 +0000

    added question three

--- js/questionAnsList.js
+++ js/questionAnsList.js
@@ -10,5 +10,24 @@ let questionList = {
   two: {
     question: 'Q2',
     questionTwochoices: ['a2, b2, c2, d2']
+  },
+
+
+  three: {
+    question: 'You have declared a function that takes in an array of numbers and returns the sum of those numbers. How do you get your function to run?',
+    questionTwochoices: ['You send your entire fist through your monitor.', 'You have to get on spotify and play "Call Me, Maybe" by Carly Rae Jepsen on repeat.', 'The code inside of the function is automatically executed after the function has been defined.', 'The function must be called upon with a given set of arguments.'],
+    correct: 'The function must be called upon with a given set of arguments.'
+  },
+
+
+  four: {
+    question: 'Q4',
+    questionTwochoices: ['a2, b2, c2, d2']
+  },
+
+
+  five: {
+    question: 'Q5',
+    questionTwochoices: ['a2, b2, c2, d2']
   }
 };
