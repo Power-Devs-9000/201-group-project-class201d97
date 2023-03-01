ca2ce43bd25bdbd710ed289a5ad6952415fa62cb
ReeceRenninger <renningerreece@gmail.com>
2023-02-28 17:44:55 -0800

    updating score-board js with single quotes

--- js/score-board.js
+++ js/score-board.js
@@ -1,14 +1,14 @@
 
-/******GLOBALS w/ DOM*******/
+// ******GLOBALS w/ DOM*******/
 
-let highScoresList = document.getElementById("highScoreList");
-let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-let finalScore = document.getElementById("finalScore");
+let highScoresList = document.getElementById('highScoreList');
+let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+let finalScore = document.getElementById("finalScore');
 let recentScore = localStorage.getItem("recentScore");
 
 
 highScoresList.innerHTML = highScores
-  .map(score => { 
+  .map(score => {
     return `<li class="high-score">${score.name} scored ${score.score}</li>`;
   })
   .join("");
